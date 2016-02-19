eb2d71e0e182db0fa8db885ae6bdff8a411fc6a4
Kyra Crowston <kyra.crow00@icloud.com>
2016-02-19 12:01:16 -0600

    edits to Gruntfile, commented out sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,50 +15,53 @@ module.exports = function(grunt) {
                 options: {
                     spawn: false
                 }
-            },
-            css: {
-                files: ['client/sass/*.scss'],
-                tasks: ['sass'],
-                options: {
-                    spawn: false
-                }
-            },
-            copy: {
-                main: {
-                    expand: true,
-                    cwd: "node_modules/",
-                    src: [ //CHANGE THESE BASED ON WHAT IS BEING USED
-                        "angular/angular.min.js",
-                        "angular/angular.min.js.map",
-                        "angular/angular-csp.css",
-                        "angular-route/angular-route.min.js",
-                        "bootstrap/*.*",
-                        "bootstrap/**/*.*"
-                    ],
-                    "dest": "server/public/vendor/"
-                }
-            },
-            sass: {
-                dist: {
-                    files: [{
-                        expand: true,
-                        cwd: 'client/sass',
-                        src: ['style.scss'],
-                        dest: 'server/public/assets/styles',
-                        ext: '.css'
-                    }]
-                }
+            }
+        },
+        //css: {
+        //    files: ['client/sass/*.scss'],
+        //    tasks: ['sass'],
+        //    options: {
+        //        spawn: false
+        //    }
+        //},
+        copy: {
+            main: {
+                expand: true,
+                cwd: "node_modules/",
+                src: [ //CHANGE THESE BASED ON WHAT IS BEING USED
+                    "angular/angular.min.js",
+                    "angular/angular.min.js.map",
+                    "angular/angular-csp.css",
+                    "angular-route/angular-route.min.js",
+                    "bootstrap/*.*",
+                    "bootstrap/**/*.*"
+                ],
+                "dest": "server/public/vendor/"
             }
         }
-    })
+        //sass: {
+        //    dist: {
+        //        files: [{
+        //            expand: true,
+        //            cwd: 'client/sass',
+        //            src: ['style.scss'],
+        //            dest: 'server/public/assets/styles',
+        //            ext: '.css'
+        //        }]
+        //    }
+        //}
+
+    });
 
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-sass');
+    //grunt.loadNpmTasks('grunt-contrib-sass');
 
 
     // Default task(s).
-    grunt.registerTask('default', ['copy', 'uglify', 'sass']);
+    grunt.registerTask('default', ['copy', 'uglify']);
+    //grunt.registerTask('default', ['copy', 'uglify', 'sass']);
+
 
 };
