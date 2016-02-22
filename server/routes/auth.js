dacf5954813041721aed18b143f6be07314de48a
cwgraff <cwgraff@yahoo.com>
2016-02-22 12:48:39 -0600

    returns a profile from Google

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -32,33 +32,36 @@ passport.use('google', new GoogleStrategy({
     process.nextTick(function () {
 
         // try to find the user based on their google id
-        User.findOne({'google.id': profile.id}, function (err, user) {
-            if (err)
-                return done(err);
-
-            if (user) {
-
-                // if a user is found, log them in
-                return done(null, user);
-            } else {
-                // if the user isnt in our database, create a new user
-                var newUser = new User();
-
-                // set all of the relevant information
-                newUser.google.id = profile.id;
-                newUser.google.token = token;
-                newUser.google.name = profile.displayName;
-                newUser.google.email = profile.emails[0].value; // pull the first email
-
-                // save the user
-                newUser.save(function (err) {
-                    if (err)
-                        throw err;
-                    return done(null, newUser);
-                });
-            }
-        });
+        // Check DB for user and authenticate or add user to DB if not there
+        console.log('Return Sent from Google, into callback function', profile);
     });
+        //User.findOne({'google.id': profile.id}, function (err, user) {
+        //    if (err)
+        //        return done(err);
+        //
+        //    if (user) {
+        //
+        //        // if a user is found, log them in
+        //        return done(null, user);
+        //    } else {
+        //        // if the user isnt in our database, create a new user
+        //        var newUser = new User();
+        //
+        //        // set all of the relevant information
+        //        newUser.google.id = profile.id;
+        //        newUser.google.token = token;
+        //        newUser.google.name = profile.displayName;
+        //        newUser.google.email = profile.emails[0].value; // pull the first email
+        //
+        //        // save the user
+        //        newUser.save(function (err) {
+        //            if (err)
+        //                throw err;
+        //            return done(null, newUser);
+        //        });
+        //    }
+        //});
+    //});
 
 }));
 
