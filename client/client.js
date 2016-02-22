294e8e9c5307b3a80c46c8cb7e376d17f8e2b082
Kyra Crowston <kyra.crow00@icloud.com>
2016-02-22 13:38:43 -0600

    successfully linked client to server to database

--- client/client.js
+++ client/client.js
@@ -6,14 +6,16 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
     //$urlRouterProvider.otherwise("/");
     $stateProvider
         .state('login', {
-        templateUrl: 'views/login.html',
-        controller: 'loginController',
-        controllerAs: 'login'
-    });
-    //$urlRouterProvider.html5Mode(true);
+            templateUrl: 'views/login.html',
+            controller: 'loginController',
+            controllerAs: 'login'
+              })
+                .state('my_speeches', {
+                    templateUrl: 'views/my_speeches.html',
+                    controller: 'SpeechHistory',
+                    controllerAs: 'history'
+                });
 }]);
-
-
     //app.controller('mainController', ['$scope', function ($scope) {
     //    //var main = this;
     //    $scope.sampleMessage = 'Angular hooked up';
@@ -26,5 +28,12 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
             .message='Hello and things!'
     });
 
-
+    app.controller('SpeechHistory', ['$http', function ($http) {
+        var history=this
+            .speeches = [];
+    $http.get('/mySpeeches').then(function(response){
+        this.speeches = response.data;
+        console.log("please!");
+    });
+}]);
 
--- client/controllers.js
+++ client/controllers.js
@@ -1,17 +1,17 @@
 //We will eventually have multiple controller files.
 
-app.controller('HistoryController', ['$http', '$scope', function ($http, $scope) {
-    $scope.history = [];
-    $http.get('/history').then(function(response){
-        $scope.tasks = response.data;
-    });
-}]);
+//app.controller('SpeechHistory', ['$http', '$scope', function ($http, $scope) {
+//    $scope.speeches = [];
+//    $http.get('/mySpeeches').then(function(response){
+//        $scope.speeches = response.data;
+//    });
+//}]);
 
-app.controller('SelectHistoryController', ['$http', '$scope', function ($http, $scope) {
-    $scope.oldHistory = [];
-    $scope.getOldDates = function(){
-        $http.post('/selectHistory', {startDate: $scope.startDate}).then(function (response) {
-            $scope.oldHistory = response.data;
-        });
-    };
-}]);
+//app.controller('SelectHistoryController', ['$http', '$scope', function ($http, $scope) {
+//    $scope.oldHistory = [];
+//    $scope.getOldDates = function(){
+//        $http.post('/selectHistory', {startDate: $scope.startDate}).then(function (response) {
+//            $scope.oldHistory = response.data;
+//        });
+//    };
+//}]);
