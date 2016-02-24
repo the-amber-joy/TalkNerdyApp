cbe1406de9ab1bff71429c983df5e5b352bae7d9
cwgraff <cwgraff@yahoo.com>
2016-02-24 09:07:58 -0600

    edits comment in login.js

--- server/routes/login.js
+++ server/routes/login.js
@@ -3,7 +3,7 @@ var path = require('path');
 
 var router = app.Router();
 
-//Serve index page back on basic root Get request
+//Serve login page back on basic root Get request
 router.get('/', function(request, response){
     response.sendFile(path.join(__dirname, '../public/views/login.html'));
 });
