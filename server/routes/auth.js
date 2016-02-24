2d1a4989dc0350e9c527c365789a5c9eac36070a
wallacewylie <wallace.wylie@gmail.com>
2016-02-24 08:41:48 -0600

    Button/Login Fix

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -100,7 +100,7 @@ router.get('/google', passport.authenticate('google', { scope : ['profile', 'ema
 //    }));
 
 router.get('/google/callback',
-    passport.authenticate('google', { failureRedirect: '/auth/login' }),
+    passport.authenticate('google', { succesRedirect: '/google/auth', failureRedirect: '/auth/login' }),
     function(req, res) {
         // Successful authentication, redirect home.
         console.log('success');
