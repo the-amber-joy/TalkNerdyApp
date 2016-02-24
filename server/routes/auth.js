76e2f7f009fe5b10141ddde1131e7618ad9cd39b
cwgraff <cwgraff@yahoo.com>
2016-02-23 20:20:27 -0600

    serves back index page with Home partial on successful log-in

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -93,22 +93,22 @@ router.get('/login', function(request, response){
 router.get('/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
 //
 // the callback after google has authenticated the user
-//router.get('/google/callback',
-//    passport.authenticate('google', {
-//        successRedirect : '/home',
-//        failureRedirect : '/login'
-//    }));
-
 router.get('/google/callback',
-    passport.authenticate('google', { failureRedirect: '/auth/login' }),
-    function(req, res) {
-        // Successful authentication, redirect home.
-        console.log('success');
-        res.redirect('/auth/home');
-    });
+    passport.authenticate('google', {
+        successRedirect : '/index',
+        failureRedirect : '/login'
+    }));
 
-router.get('/google/callback', function(request, response){
-    response.sendFile(path.join(__dirname, '../public/views/home.html'));
-});
+//router.get('/google/callback',
+//    passport.authenticate('google', { failureRedirect: '/auth/login' }),
+//    function(req, res) {
+//        // Successful authentication, redirect home.
+//        console.log('success');
+//        res.redirect('/index');
+//    });
+
+//router.get('/google/callback', function(request, response){
+//    response.sendFile(path.join(__dirname, '../public/views/home.html'));
+//});
 
 module.exports = router;
